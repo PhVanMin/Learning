44559407d10fb2d7bf39df28341f1170a2d309ed
PhVanMin <phamminh3107@gmail.com>
2024-06-03 15:48:04 +0700

    init repo

---
+++ C/shell/lsh.h
@@ -0,0 +1,12 @@
+#ifndef LSH_H
+#define LSH_H
+
+#define STATUS_RUNNING true
+#define STATUS_STOP false
+#define LSH_BUFSIZE 1024
+#define LSH_TOK_BUFSIZE 64
+#define LSH_TOKEN_DELIM " \t\r\n\a"
+
+void lsh_loop();
+
+#endif
